dad0d15897b083eca3970dc14a67a63c6e3aec92
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-12-27 00:59:30 +0200

    add deploy workflow and bug fixes

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -2,7 +2,6 @@
 
 import { getProjects } from '../api';
 import { getQuote } from '../api';
-import { getReadme } from '../api';
 import { getWeather } from '../api';
 import config from '../../../config.json';
 
